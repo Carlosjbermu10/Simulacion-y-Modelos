a57f098a84e4030a46b52ac4b3a94ca588a41247
Carlosjbermu10 <carlosjbermu10@gmail.com>
2023-01-14 16:47:12 -0400

    ahora lee las estaturas en forma de columnas

--- descriptiva datos/descriptiva_datos.py
+++ descriptiva datos/descriptiva_datos.py
@@ -7,34 +7,40 @@ import csv
 
 def descriptiva_datos(file):
     with open(file, 'r') as f:
-        lector = csv.reader(f, delimiter = ",")
+        
+        lista = []
+        alturas = []
+        lector = csv.reader(f)
         # omite el encabezado
         next(lector, None)
-        for fila in lector:
-            estatura1 = float(fila[0])
 
-        for i in range(len(fila)):
-            fila[i] = float(fila[i])
-        
-        print(f"Datos: {fila}")
+        for fila in lector:
+            lista.append(list(map(float, fila)))
+
+        for x in lista:
+            alturas += x
+
+        print(f"Datos: {lista}")
+
+        print(f"Datos: {alturas}")
         print()
-        fila.sort()
-        print(f"1.Ordenar los datos(de menor a mayor): {fila}")
+        alturas.sort()
+        print(f"1.Ordenar los datos(de menor a mayor): {alturas}")
         print()
         print("2.Medidas de Tendencia central")
-        media = st.mean(fila) #media 
+        media = st.mean(alturas) #media 
         print(f"La media es: {media}")
-        moda = st.mode(fila) #moda 
+        moda = st.mode(alturas) #moda 
         print(f"La mediana es: {moda}")
-        mediana = st.median(fila) #mediana
+        mediana = st.median(alturas) #mediana
         print(f"La moda es: {mediana}")
         print()
         print("3.Medidas de dispersion")
-        varianza = st.variance(fila) #varianza
+        varianza = st.variance(alturas) #varianza
         print(f"La varianza es: {varianza}")
-        des_est = st.stdev(fila) #desviacion estandar
+        des_est = st.stdev(alturas) #desviacion estandar
         print(f"La desviacion estandar es: {des_est}")
-        rango = fila[len(fila) - 1] - fila[0]  #rango
+        rango = alturas[len(alturas) - 1] - alturas[0]  #rango
         print(f"El rango es: {rango}")
 
 if __name__ == "__main__":
